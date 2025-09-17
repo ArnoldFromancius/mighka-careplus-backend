566716062e433390cd85ca834092939304baa091
arnoldfromancius <arnoldfromancius@gmail.com>
2025-09-17 10:23:19 +0200

    installed express

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
-const PORT = process.env.PORT || 5000;
+// server.js
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Hello from backend 👋");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
 
