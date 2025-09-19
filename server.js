f2d5bce86ac799c60b070817a23e15b2a3731ea6
arnoldfromancius <arnoldfromancius@gmail.com>
2025-09-19 14:01:10 +0200

    minor fixes

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ app.use("/jobs", jobRoutes);
 
 
 // Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log(" Connected to MongoDB Atlas"))
 .catch(err => {
   console.error(" MongoDB connection error:", err);
